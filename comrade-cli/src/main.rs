a2250e563bfd8ba6fbdd3fa18c9727076eda3d33
Donald Stufft <donald@stufft.io>
2022-05-20 00:12:06 -0400

    Setup anyhow and ctrlc

--- comrade-cli/src/main.rs
+++ comrade-cli/src/main.rs
@@ -1,13 +1,26 @@
+use anyhow::Result;
 use comrade::logwatch::LogManager;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::{thread, time};
 
 const LOGPATH: &str = r"logfile";
 
-fn main() {
+fn main() -> Result<()> {
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let mut manager = LogManager::new();
     manager.add(LOGPATH);
 
-    loop {
+    while running.load(Ordering::SeqCst) {
         thread::sleep(time::Duration::from_secs(1))
     }
+
+    Ok(())
 }
